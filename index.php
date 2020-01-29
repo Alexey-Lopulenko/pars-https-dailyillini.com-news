e3908bf52ca49c2fad169987fb2afbc5e397094f
Alexey-Lopulenko <alexlojlua@gmail.com>
2020-01-29 15:11:26 +0200

    all completed

--- index.php
+++ index.php
@@ -1,52 +1,119 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>Pars http://dailyillini.com/category/news/ </title>
+    <meta charset="utf-8">
+    <title>pars</title>
 </head>
 <body>
-<center>
-    <h1>
-        result pars site - http://dailyillini.com/category/news/ 
-    </h1>
-    <hr>
-</center>
-<div class="content">
-    <div class="row mb-2">
-        <?php
-        require_once 'setting.php';
-        $data = $pdo->query("SELECT * FROM news ORDER BY created_at ASC")->fetchAll();
-        $i = 1;
-        foreach ($data as $row):
-            ?>
-            <div class="col-md-12">
-                <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col-md-8">
-                        <div class="col p-4 d-flex flex-column position-static">
-                            <h3 class="mb-0"><?= $row['h1'] ?></h3>
-                            <div class="mb-1 text-muted"><?= $row['created'] ?></div>
-                            <p class="card-text mb-auto"><?= $row['text'] ?></p>
-                            <a href="<?= $row['url_page'] ?>"
-                               class="stretched-link" <?php echo $i <= 3 ? 'style="color: red"' : '' ?>>Page news</a>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <?php if($row['url_img_in_site'] != 'no image!'):?>
-                        <img src="<?= $row['url_img_in_site'] ?>" alt="" style="max-height: 400px;">
-                        <?php else:?>
-                            <img src="http://nitmeghalaya.in/nitmeghalaya/htmls/images/defaultuser.jpg" alt="No image" style="max-height: 400px;min-width: 550px;">
-                        <?php endif;?>
-                    </div>
-                </div>
-            </div>
-            <?php $i++; ?>
-        <?php endforeach; ?>
-    </div>
-</div>
+<h1>
+    <center>pars news</center>
+</h1>
+<hr>
+<form action="result.php" method="post">
+    <p>Site: <input name="site" type="url" value="https://dailyillini.com/news/"></p>
+    <p><input type="submit" value="Enter"></p>
+
+</form>
 </body>
 </html>
+
+<?php
+ini_set("max_execution_time", 360);
+require_once 'classes/parsing.php';
+require_once 'setting.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $siteUrl = $_POST['site'];
+
+    if ($siteUrl) {
+        $pars = new parsing();
+        $pars->setCurrentUrl($siteUrl);
+        $pars->setInternalLinks();
+        $arrLinksNews = $pars->getNewsLinks();
+        echo '<pre>';
+        print_r($arrLinksNews);
+        echo '</p re>';
+
+        $sql = "INSERT INTO news (h1, text, created, url_img_in_site, url_page, path_img_in_my_server,created_at) VALUES (?,?,?,?,?,?,?)";
+
+        $i = 0;
+        $countPage = 1;
+
+        while ($arrLinksNews) {
+            $newLink = new parsing();
+            $newLink->setCurrentUrl($arrLinksNews[$i]);
+
+            $dateCreated = $newLink->getDateCreateNews();
+            $h1 = $newLink->getHeadingNews();
+            $textNews = $newLink->getTextNews();
+            $url_img_in_site = $newLink->getImageUrl();
+            $path_img_in_my_server = $newLink->saveImage($url_img_in_site);
+            $today = getdate();
+
+            if (!($url_img_in_site)) {
+                $url_img_in_site = 'no image!';
+            }
+
+            echo '<hr>';
+            echo '<pre>';
+            print_r($h1);
+            echo '<br>';
+            echo '<pre>';
+            print_r($textNews);
+            echo '<br>';
+            print_r($url_img_in_site);
+            echo '<br>';
+            print_r($path_img_in_my_server);
+            echo '</pre>';
+            echo '<hr>';
+
+            $stmt = $pdo->prepare('SELECT * FROM news WHERE h1 = :h1');
+            $stmt->execute(['h1' => $h1]);
+            $row = $stmt->fetch();
+
+            //save in db
+            if (!$row) {
+                $pdo->prepare($sql)->execute([$h1, $textNews, $dateCreated, $url_img_in_site, $arrLinksNews[$i], $path_img_in_my_server, $today[0]]);
+            } else {
+                $sql = "UPDATE news SET created_at=? WHERE h1=?";
+                $pdo->prepare($sql)->execute([$today, $h1]);
+            }
+
+            $pars->addInternalLink($arrLinksNews[$i]);
+            $pars->setVisitedPages($arrLinksNews[$i]);
+            $pars->deleteInternalLinks($arrLinksNews[$i]);
+
+//            if ($i+1 == 3) {
+//                break;
+//            }
+            if ($i + 1 == count($arrLinksNews)) {
+
+                $i = 0;
+                $nextPage = $nextPage = $pars->nextPage();
+                if ($countPage == 3 || $nextPage == "No next page") {
+                    echo '<strong>Limit</strong>';
+                    break;
+                }
+                echo '<hr><pre>';
+                print_r($nextPage);
+                echo '</pre><hr>';
+                $pars->setVisitedPagesInNull();
+                $pars->setCurrentUrl($nextPage);
+                $pars->setInternalLinks();
+                $arrLinksNews = [];
+                $arrLinksNews = $pars->getNewsLinks();
+
+                echo '<pre>';
+                print_r($arrLinksNews);
+                echo '</pre><hr>';
+
+                $countPage++;
+            }
+            $i++;
+        }
+    }
+}
+
+
+?>
